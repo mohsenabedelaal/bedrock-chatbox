cea881fbd3b84f2aa4f76e81a0880f455f3c571e
mohsen-Incrowd <mohsen.abdelaal@Incrowdnow.com>
2024-08-09 14:52:54 +0300

    Initial commit

---
+++ test/bedrock-chatbox-cdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const BedrockChatboxCdk = require('../lib/bedrock-chatbox-cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bedrock-chatbox-cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new BedrockChatboxCdk.BedrockChatboxCdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
